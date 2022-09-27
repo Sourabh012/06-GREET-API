aee4e7edbe2cde6aa5acb138bd8b2ed372bdca22
Sourabh012 <saurabhsaraswat54@gmail.com>
2022-09-27 10:21:20 +0530

    first commit

---
+++ src/main/java/com/accenture/GreetRestController.java
@@ -0,0 +1,17 @@
+package com.accenture;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+	
+	
+	@GetMapping("/greet")
+	public String getGreetMsg()
+	{
+		String msg="Good Evening..!!";
+		return msg;
+	}
+
+}
